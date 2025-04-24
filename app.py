56262299404dbe7c049fc5eee94671fa3e731037
Deepak <deepakraja007.dr@gmail.com>
2025-04-24 18:41:06 -0400

    Update code

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Fix for SQLite version issues with ChromaDB
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import chromadb
 import os
